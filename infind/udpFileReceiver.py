405f56d14202ff4f28ec6858b628e4e0ccae4cc4
Gregory Chagas <gregory.chagas@ifsc.edu.br>
2020-03-20 13:52:06 -0300

    tentativa de loop inf

--- infind/udpFileReceiver.py
+++ infind/udpFileReceiver.py
@@ -9,23 +9,24 @@ import select
 host = socket.gethostbyaddr("34.210.45.213")[0]
 port = 7000
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host, port))
+while True:
+    s.bind((host, port))
 
-addrHost = (host, port)
-buf=1024
+    addrHost = (host, port)
+    buf=1024
 
-fileName, addrHost = s.recvfrom(buf)
-print ("Received File:",fileName.strip())
+    fileName, addrHost = s.recvfrom(buf)
+    print ("Received File:",fileName.strip())
 
-f = open(fileName.strip(),'wb')
+    f = open(fileName.strip(),'wb')
 
-data, addrHost = s.recvfrom(buf)
-try:
-    while(data):
-        f.write(data)
-        s.settimeout(2)
-        data, addrHost = s.recvfrom(buf)
-except timeout:
-    f.close()
-    s.close()
-    print ("File Downloaded")
+    data, addrHost = s.recvfrom(buf)
+    try:
+        while(data):
+            f.write(data)
+            s.settimeout(2)
+            data, addrHost = s.recvfrom(buf)
+    except timeout:
+        f.close()
+        s.close()
+        print ("File Downloaded")
