50b2669bfc4333d55997e67652bb6992d341721e
Gregory Chagas <gregory.chagas@ifsc.edu.br>
2020-03-20 15:31:25 -0300

    Update udpFileReceiver.py

--- infind/udpFileReceiver.py
+++ infind/udpFileReceiver.py
@@ -21,7 +21,7 @@ while True:
     #print ("Received File:",fileName.strip())
 
     fileName = "aula.txt"
-    f = open(fileName.strip(),'w')
+    f = open(fileName.strip(),'x')
 
     data, addrHost = s.recvfrom(buf)
     try:
